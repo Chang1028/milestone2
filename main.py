83b4b545a2dbe7b35b55dc016208e5f72d8e3746
Chang1028 <xue6bq@virginia.edu>
2023-04-19 14:52:26 -0400

    smoking data added

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
+# import packages
+import torch
+import numpy as np
+from torch.utils.data import Dataset, DataLoader
+from torchvision import datasets
+from torchvision.transforms import ToTensor, Lambda
+import matplotlib.pyplot as plt
+from torch import nn
 
