aee42a32c8116fbc54bfd469485a677f60eda13b
Chang1028 <xue6bq@virginia.edu>
2023-04-23 19:10:06 -0400

    smoking data added

--- pytorch-example-logit.py
+++ pytorch-example-logit.py
@@ -1,83 +1,74 @@
-# import packages
+# %%
+# load package
 import torch
 import numpy as np
-import pandas as pd
-from sklearn.model_selection import train_test_split
 from torch.utils.data import Dataset, DataLoader
 from torchvision import datasets
-from torchvision.transforms import ToTensor
+from torchvision.transforms import ToTensor, Lambda
 import matplotlib.pyplot as plt
 from torch import nn
 
-input_size = 24
+# %%
+# load data
+local_folder = '/Users/changli/Desktop/UVA/STAT5630 Machine Learning/project/milestone2/pytorch-example'
+# new_mirror = 'https://ossci-datasets.s3.amazonaws.com/mnist'
+# datasets.MNIST.resources = [
+#    ('/'.join([new_mirror, url.split('/')[-1]]), md5)
+#    for url, md5 in datasets.MNIST.resources
+# ]
+training_data = datasets.MNIST(
+    root=local_folder,
+    train=True,
+    download=True,
+    transform=ToTensor()
+)
+
+test_data = datasets.MNIST(
+    root=local_folder,
+    train=False,
+    download=True,
+    transform=ToTensor()
+)
+
+training_one = training_data[100]
+training_one = training_one[0]
+input_size = training_one.shape[0] * training_one.shape[1] * training_one.shape[2]
+
+# %%
+# algorithm optimization
 batch_size = 64
-
-# data preprocess
-data = pd.read_csv("smoking.csv")
-data = data.drop(["ID", "oral"], axis=1)
-data = pd.get_dummies(data, drop_first=True)
-training_data, test_data = train_test_split(data, test_size=0.2)
-training_data = training_data.reset_index(drop=True)
-test_data = test_data.reset_index(drop=True)
-
-class SmokeDataset(Dataset):
-    def __init__(self, dataset, label="smoking"):
-        self.data = dataset
-        self.labels = self.data[label]
-        self.data = self.data.drop([label], axis = 1)
-
-
-    def __len__(self):
-        return len(self.labels)
-
-    def __getitem__(self, idx):
-        observation = self.data.iloc[idx, :]
-        label = self.labels.iloc[idx]
-        observation = torch.tensor(observation, dtype=torch.float32)
-        label = torch.tensor(label, dtype=torch.long)
-        return observation, label
-
-
-training_data = SmokeDataset(training_data)
-test_data = SmokeDataset(test_data)
-
-
 train_dataloader = DataLoader(
-    dataset = training_data,
-    batch_size = batch_size,
-    shuffle = True
-)
-test_dataloader = DataLoader(
-    dataset = test_data,
-    batch_size = batch_size,
-    shuffle = True
+    dataset=training_data, batch_size=batch_size,
+    shuffle=True
 )
 
-num_classes = 2
+test_dataloader = DataLoader(
+    test_data,
+    batch_size=batch_size,
+    shuffle=True
+)
+# %%
+# define model
+num_classes = 10
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 print('Using {} device'.format(device))
-
-
-class SmokeNetwork(nn.Module):
-    def __init__(self, *args, **kwargs):
-        super(SmokeNetwork, self).__init__()
-        self.linear_relu = nn.Sequential(
-            nn.Linear(input_size, 16),
-            nn.Sigmoid(),
-            nn.Linear(16, num_classes),
-        )
-
+class NeuralNetwork(nn.Module):
+    def __init__(self):
+        super(NeuralNetwork, self).__init__()
+        self.flatten = nn.Flatten() # change image to a vector
+        self.linear_relu_stack = nn.Linear(input_size, num_classes)
 
     def forward(self, x):
-        logits = self.linear_relu(x)
+        x = self.flatten(x)
+        logits = self.linear_relu_stack(x)
         return logits
-
-
-model = SmokeNetwork().to(device)
+# model.eval()
+# %%
+# Optimizing the Model Parameters
+model = NeuralNetwork().to(device)
 model.train()
 loss_fn = nn.CrossEntropyLoss()
 optimizer = torch.optim.SGD(model.parameters(), lr=0.1)
-
 
 def train(dataloader, model, loss_fn, optimizer):
     size = len(dataloader.dataset)
@@ -93,14 +84,13 @@ def train(dataloader, model, loss_fn, optimizer):
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
-
+        
         if batch % 100 == 0:
             loss, current = loss.item(), batch * len(X)
             loss_list.append(loss)
             print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
-
-    return loss_list
-
+    
+    return loss_list  
 
 def test(dataloader, model):
     size = len(dataloader.dataset)
@@ -114,20 +104,19 @@ def test(dataloader, model):
             correct += (pred.argmax(1) == y).type(torch.float).sum().item()
     test_loss /= size
     correct /= size
-    print(f"Test Error: \n Accuracy: {(100 * correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+    print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
     return test_loss
-
-
-epochs = 40
+# %%
+epochs = 20 
 test_loss_list = []
 train_loss_list = []
 for t in range(epochs):
     print(f"Epoch {t+1}\n-------------------------------")
     train_loss_list.append(
         train(train_dataloader, model, loss_fn, optimizer))
-    test_loss_list.append(test(test_dataloader, model))
+    test_loss_list.append(test(test_dataloader, model)) 
 print("Done!")
-
+# %%
 # loss function curve
 flat_train_loss_list = [item for sublist in train_loss_list for item in sublist]
 plt.plot(np.linspace(0, len(flat_train_loss_list), len(flat_train_loss_list)), np.array(flat_train_loss_list))
